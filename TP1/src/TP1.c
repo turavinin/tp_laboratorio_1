8dd1854220d25ad44dffe05a272854aa89e57896
turavinin <aturavinin@gmail.com>
2021-04-17 15:04:13 -0300

    Primera funcion

--- TP1/src/TP1.c
+++ TP1/src/TP1.c
@@ -1,17 +1,17 @@
-/*
- ============================================================================
- Name        : TP1.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
-
 #include <stdio.h>
 #include <stdlib.h>
+#include "mensajesEnConsola.h"
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	mostrarMensaje("¡Bienvenido al Calculador 3000!\nTodos tus calculos se harán realidad.");
+
+
+
+
+
+
+
+
+
 	return EXIT_SUCCESS;
 }
---
+++ TP1/src/mensajesEnConsola.h
@@ -0,0 +1,9 @@
+#ifndef MENSAJESENCONSOLA_H_
+#define MENSAJESENCONSOLA_H_
+
+/* brief Muestra el mensaje pasado por parametro en consola
+ * param1 Recibe el mensaje
+ * */
+void mostrarMensaje(char* mensaje);
+
+#endif /* MENSAJESENCONSOLA_H_ */
