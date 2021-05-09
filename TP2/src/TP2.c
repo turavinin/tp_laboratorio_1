6e50091035db1645cd6d033db37b9f2ae6571cb0
turavinin <aturavinin@gmail.com>
2021-05-09 19:36:43 -0300

    Creado entorno TP2

---
+++ TP2/src/TP2.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : TP2.c
+ Author      : Anton Turavinin
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Trabajo Practico N° 2
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
